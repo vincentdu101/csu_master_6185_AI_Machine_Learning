1a11293a56553bec4978b5011734597eff8f8dad
vincentdu101 <vincentdu101@gmail.com>
2018-04-26 05:54:00 -0700

    fixed labeling and outputted top 3 chromosomes

--- assignment_4_genetic_algorithms/chromosomes_max_fitness.py
+++ assignment_4_genetic_algorithms/chromosomes_max_fitness.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
-import numpy
+import numpy as np
 
 from deap import base
 from deap import creator
@@ -10,14 +10,14 @@ from deap import tools
 # individual specifies the groups of data input representing each 
 # group of chromosomes in array of array
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", numpy.ndarray, fitness=creator.FitnessMax)
+creator.create("Chromosome", list, fitness=creator.FitnessMax)
 
 # specifies the toolbox and the population data generation settings
 # populates groups with random int data between 0 and 9
 toolbox = base.Toolbox()
 toolbox.register("attr_int", random.randint, 0, 9)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_int, 100)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("chromosome", tools.initRepeat, creator.Chromosome, toolbox.attr_int, 8)
+toolbox.register("population", tools.initRepeat, list, toolbox.chromosome)
 
 # fitness function that determines the output value of the input group of 
 # chromosomes
@@ -30,15 +30,15 @@ def evaluateFitnessFunction(ind):
 toolbox.register("evaluate", evaluateFitnessFunction)
 toolbox.register("mate", tools.cxTwoPoint)
 toolbox.register("mutate", tools.mutFlipBit, indpb = 0.05)
-toolbox.register("select", tools.selTournament, tournsize = 3)
+toolbox.register("select", tools.selBest)
 
 def main():
     textfile = open("chromosomes_max_fitness_output.txt", "w")
     
-    random.seed(64)
+    random.seed(32)
     
     # 100 groups of random data
-    pop = toolbox.population(n = 8)
+    pop = toolbox.population(n = 100)
 
     CXPB, MUTPB = 0.5, 0.2
     
@@ -48,7 +48,7 @@ def main():
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
         
-    textfile.write("  Evaluated %i individuals\n" %len(pop))
+    textfile.write("  Evaluated %i chromosomes\n" %len(pop))
     
     # Extracting all the fitnesses of 
     fits = [ind.fitness.values[0] for ind in pop]
@@ -56,14 +56,15 @@ def main():
     # Variable keeping track of the number of generations
     g = 0
     
-    # Begin the evolution, 25 generations
-    while max(fits) < 100 and g < 25:
+    # Begin the evolution, 10 generations
+    while g < 10:
         # A new generation
         g = g + 1
-        textfile.write("-- Generation %i --\n" % g)
+        textfile.write("\n-- Generation %i --\n" % g)
         
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
+        
         # Clone the selected individuals
         offspring = list(map(toolbox.clone, offspring))
     
@@ -86,29 +87,35 @@ def main():
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
     
-        # Evaluate the individuals with an invalid fitness
+        # Evaluate the individuals with an invalid fitness with a new fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
         
-        textfile.write("  Evaluated %i individuals\n" % len(invalid_ind))
+        textfile.write("   Re-evaluated %i chromosomes\n" % len(invalid_ind))
         
         # The population is entirely replaced by the offspring
+        textfile.write("   Chromosomes in the next generation %i\n" % len(offspring))
         pop[:] = offspring
         
         # Gather all the fitnesses in one list and print the stats
         fits = [ind.fitness.values[0] for ind in pop]
         
-        fitnessVal = evaluateFitnessFunction(fits)
+        # Get the top 3 chromosomes
+        topThree = tools.selBest(pop, 3)
         
-        textfile.write("   Individual Group: %s\n" % fits)
-        textfile.write("   Fitness Value Output: %s\n" % fitnessVal)
+        textfile.write("   Order of Most Fit Chromosome: %s\n" % fits)
+        textfile.write("   Top 3 Chromosomes\n")
+        textfile.write("   1. %s\n" % topThree[0])
+        textfile.write("   2. %s\n" % topThree[1])
+        textfile.write("   3. %s\n" % topThree[2])        
+        textfile.write("   Max Fitness Value Output of population: %s\n" % max(fits))
     
     textfile.write("-- End of (successful) evolution --\n")
     
     best_ind = tools.selBest(pop, 1)[0]
-    textfile.write("Best individual is %s, %s\n" % (best_ind, best_ind.fitness.values))
+    textfile.write("\nBest chromosome is %s, %s\n" % (best_ind, best_ind.fitness.values))
 
 if __name__ == "__main__":
     main()
